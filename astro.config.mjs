c7e782f08690e2bd3482767c81c541fe46a0dfc2
Luis Arteaga <luis.arteaga@cobistopaz.com>
2025-11-20 20:57:40 -0500

    feat: Añade la sección de Preguntas Frecuentes, actualiza el idioma del sitio a español, el dominio y una herramienta de IaC.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://meruemart.com',
+    site: 'https://meruemart.cloud',
     integrations: [sitemap()],
 });
